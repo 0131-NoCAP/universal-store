200a99741e7af77340eceb46daaba5d788fd0547
Jeremy Zou <jeremy.zou@gmail.com>
2020-09-03 16:07:42 -0400

    Completed UI for login page

--- application/App.js
+++ application/App.js
@@ -5,23 +5,58 @@ import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import BottomTabNavigator from './navigation/BottomTabNavigator';
 import useLinking from './navigation/useLinking';
 
+import LoginScreen from './screens/LoginScreen';
+
 const Stack = createStackNavigator();
+const AuthContext = React.createContext();
 
 export default function App(props) {
   const [isLoadingComplete, setLoadingComplete] = React.useState(false);
   const [initialNavigationState, setInitialNavigationState] = React.useState();
   const containerRef = React.useRef();
   const { getInitialState } = useLinking(containerRef);
+  const [state, dispatch] = React.useReducer(
+    (prevState, action) => {
+      switch (action.type) {
+        case 'RESTORE_TOKEN':
+          return {
+            ...prevState,
+            userToken: action.token,
+            isLoading: false,
+          };
+        case 'SIGN_IN':
+          return {
+            ...prevState,
+            isSignout: false,
+            userToken: action.token,
+          };
+        case 'SIGN_OUT':
+          return {
+            ...prevState,
+            isSignout: true,
+            userToken: null,
+          };
+      }
+    },
+    {
+      isLoading: true,
+      isSignout: false,
+      userToken: null,
+    }
+  );
 
   // Load any resources or data that we need prior to rendering the app
   React.useEffect(() => {
     async function loadResourcesAndDataAsync() {
+      let userToken;
       try {
         SplashScreen.preventAutoHide();
+        userToken = await AsyncStorage.getItem('userToken');
 
         // Load our initial navigation state
         setInitialNavigationState(await getInitialState());
@@ -36,6 +71,7 @@ export default function App(props) {
         // We might want to provide this error information to an error reporting service
         console.warn(e);
       } finally {
+        dispatch({ type: 'RESTORE_TOKEN', token: userToken });
         setLoadingComplete(true);
         SplashScreen.hide();
       }
@@ -44,19 +80,57 @@ export default function App(props) {
     loadResourcesAndDataAsync();
   }, []);
 
+  const authContext = React.useMemo(
+    () => ({
+      signIn: async data => {
+        // In a production app, we need to send some data (usually username, password) to server and get a token
+        // We will also need to handle errors if sign in failed
+        // After getting token, we need to persist the token using `AsyncStorage`
+        // In the example, we'll use a dummy token
+
+        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
+      },
+      signOut: () => dispatch({ type: 'SIGN_OUT' }),
+      signUp: async data => {
+        // In a production app, we need to send user data to server and get a token
+        // We will also need to handle errors if sign up failed
+        // After getting token, we need to persist the token using `AsyncStorage`
+        // In the example, we'll use a dummy token
+
+        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
+      },
+    }),
+    []
+  );
+
   if (!isLoadingComplete && !props.skipLoadingScreen) {
     return null;
   } else {
     console.log(process.env.REACT_APP_API_KEY);
     return (
-      <View style={styles.container}>
+      <AuthContext.Provider value={authContext}>
         {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
         <NavigationContainer ref={containerRef} initialState={initialNavigationState}>
           <Stack.Navigator>
-            <Stack.Screen name="Root" component={BottomTabNavigator} />
+            {state.userToken == null ? (
+              // No token found, user isn't signed in
+              <Stack.Screen
+                name="SignIn"
+                component={LoginScreen}
+                options={{
+                  title: 'lucky',
+                  headerShown: false,
+                  // When logging out, a pop animation feels intuitive
+                  // You can remove this if you want the default 'push' animation
+                  animationTypeForReplace: state.isSignout ? 'pop' : 'push',
+                }}
+              />
+            ) : (
+              <Stack.Screen name="Root" component={BottomTabNavigator} />
+            )}
           </Stack.Navigator>
         </NavigationContainer>
-      </View>
+      </AuthContext.Provider>
     );
   }
 }
---
+++ application/screens/LoginScreen.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
+export default class LoginScreen extends React.Component {
+
+  // Instance variables
+  state = {
+    email:'',
+    setEmail:'',
+    password:'',
+    setPassword:''
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text style={styles.logo}>lucky</Text>
+        <View style={styles.inputView} >
+          <TextInput
+            style={styles.inputText}
+            placeholder="Email"
+            placeholderTextColor="#003f5c"
+            value={this.email}
+            onChangeText={this.setEmail}
+          />
+        </View>
+        <View style={styles.inputView} >
+          <TextInput
+            secureTextEntry
+            style={styles.inputText}
+            placeholder="Password"
+            placeholderTextColor="#003f5c"
+            value={this.password}
+            onChangeText={this.setPassword}
+          />
+        </View>
+        <TouchableOpacity>
+          <Text style={styles.forgot}>Forgot Password?</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.loginBtn}>
+          <Text style={styles.loginText}>Log In</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.signupText}>Sign Up</Text>
+        </TouchableOpacity>
+
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo:{
+    fontFamily: 'pacifico',
+    fontSize:120,
+    color:"#9c27b0",
+    marginBottom:60
+  },
+  inputView:{
+    width:"80%",
+    backgroundColor:"#fff",
+    borderRadius:25,
+    borderBottomColor:"black",
+    borderBottomWidth:1,
+    borderLeftColor:"black",
+    borderLeftWidth:1,
+    borderRightColor:"black",
+    borderRightWidth:1,
+    borderTopColor:"black",
+    borderTopWidth:1,
+    height:50,
+    marginBottom:20,
+    justifyContent:"center",
+    padding:20
+  },
+  inputText:{
+    height:50,
+    color:"black"
+  },
+  forgot:{
+    textAlign:"left",
+    alignSelf:"stretch",
+    color:"#27B09B",
+    fontSize:11,
+  },
+  loginBtn:{
+    width:"80%",
+    backgroundColor:"#27B09B",
+    borderRadius:25,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:40,
+    marginBottom:10
+  },
+  loginText:{
+    color:"white"
+  },
+  signupText:{
+    marginTop:20,
+    color:"#27B09B"
+  }
+});
