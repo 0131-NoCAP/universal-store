8f2c6af668dc9f35d700da4468a07acab5e14016
Jeremy Zou <jeremy.zou@gmail.com>
2020-09-04 12:24:38 -0400

    Fixed wecome to welcome in home screen

--- application/screens/HomeScreen.js
+++ application/screens/HomeScreen.js
@@ -7,11 +7,11 @@ import { MonoText } from '../components/StyledText';
 
 export default function HomeScreen() {
   return (
-    
+
     <View style={{flex: 1, alignItems:'center'}}>
       <View style={{flex: 1}}/>
       <Text style={{flex: 2, fontSize: 50, padding: 20, textAlign:'center'}}>
-        <Text>Wecome, and thank you for shopping with </Text>
+        <Text>Welcome, and thank you for shopping with </Text>
         <Text style={{fontFamily: 'pacifico', color: '#9c27b0'}}>lucky</Text>
       </Text>
       <View style={{flex: 2}}/>
