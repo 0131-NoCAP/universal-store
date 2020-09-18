afa468dfcaaf22d9ce6736346c73db01ba42a9f4
charjhall <jackson.hall@gatech.edu>
2020-09-18 13:45:47 -0400

    Remove unused imports and cleaned up code with Prettier

--- application/screens/AccountScreen.js
+++ application/screens/AccountScreen.js
@@ -1,24 +1,19 @@
-import * as WebBrowser from 'expo-web-browser';
-import * as React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
-import { AuthContext } from '../providers/auth';
+import * as React from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import { AuthContext } from "../providers/auth";
 
-import { landingPageStyles as styles} from '../constants/Styles';
+import { landingPageStyles as styles } from "../constants/Styles";
 export default function AccountScreen() {
-    const authContext = React.useContext(AuthContext)
+  const authContext = React.useContext(AuthContext);
   return (
-
-  <View style={{flex: 1, alignItems:'center'}}>
-    <View style={{flex: 2}}/>
-    <TouchableOpacity
-      onPress={() => authContext.signOut()}
-      style={styles.wideBtn}
-    >
-      <Text style={styles.buttonText}>Log Out</Text>
-    </TouchableOpacity>
-  </View>
-
-
+    <View style={{ flex: 1, alignItems: "center" }}>
+      <View style={{ flex: 2 }} />
+      <TouchableOpacity
+        onPress={() => authContext.signOut()}
+        style={styles.wideBtn}
+      >
+        <Text style={styles.buttonText}>Log Out</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
--- application/screens/HomeScreen.js
+++ application/screens/HomeScreen.js
@@ -1,24 +1,15 @@
-import * as WebBrowser from 'expo-web-browser';
-import * as React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
-
-import { landingPageStyles as styles} from '../constants/Styles';
-
-import { MonoText } from '../components/StyledText';
+import * as React from "react";
+import { Text, View } from "react-native";
 
 export default function HomeScreen() {
-
   return (
-
-    <View style={{flex: 1, alignItems:'center'}}>
-      <View style={{flex: 1}}/>
-      <Text style={{flex: 2, fontSize: 50, padding: 20, textAlign:'center'}}>
+    <View style={{ flex: 1, alignItems: "center" }}>
+      <View style={{ flex: 1 }} />
+      <Text style={{ flex: 2, fontSize: 50, padding: 20, textAlign: "center" }}>
         <Text>Welcome, and thank you for shopping with </Text>
-        <Text style={{fontFamily: 'pacifico', color: '#9c27b0'}}>lucky</Text>
+        <Text style={{ fontFamily: "pacifico", color: "#9c27b0" }}>lucky</Text>
       </Text>
-      <View style={{flex: 2}}/>
-
+      <View style={{ flex: 2 }} />
     </View>
   );
 }
