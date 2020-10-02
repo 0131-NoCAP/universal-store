b449a3aa1f4ae19e4d9ecea6c4c3b907191b4785
Jeremy Zou <jeremy.zou@gmail.com>
2020-10-02 13:42:58 -0400

    Merchants can install application

--- server/lambda_function.py
+++ server/lambda_function.py
@@ -1,4 +1,4 @@
-import json, re, requests, os
+import json, re, requests, os, random
 from secret_manager import get_secret
 
 def lambda_handler(event, context):
@@ -7,7 +7,7 @@ def lambda_handler(event, context):
         'body': json.dumps('Bad Request')
     }
 
-    print(event.get('queryStringParameters'))
+    print(event)
 
     query_string_parameters = event.get('queryStringParameters')
     if query_string_parameters:
@@ -23,6 +23,10 @@ def lambda_handler(event, context):
         timestamp = query_string_parameters.get('timestamp')
     else:
         return bad_request
+
+    if event.get('resource') == '/redirect':
+        return redirect(shop)
+
     # need to check nonce (state) with server generated install url
     # need to check if hmac is valid
     # need to check hostname parameter is valid
@@ -52,3 +56,25 @@ def post_perm_access_token(shop: str, code: str):
     except request.exceptions.RequestException as e:
         print(e)
     return output
+
+def redirect(shop: str):
+    shopify_keys = get_secret('shopify_keys')
+    if not shopify_keys:
+        print('key retrieval failed')
+        return 0
+    api_key = shopify_keys.get('shopify_client_id')
+    redirect_uri = 'https://gozbyp62l6.execute-api.us-east-2.amazonaws.com/prod'
+    access_mode = 'per-user'
+    scopes = 'read_inventory,read_orders,write_orders,read_customers,read_products,read_product_listings'
+    nonce = generate_nonce()
+    url = f'https://{shop}/admin/oauth/authorize?client_id={api_key}&scope={scopes}&redirect_uri={redirect_uri}&state={nonce}&grant_options[]={access_mode}'
+    request = {
+        'statusCode': 302,
+        'headers': {'Location': url},
+        'body': json.dumps({})
+    }
+    return request
+
+def generate_nonce(length=8):
+    """Generate pseudorandom number."""
+    return ''.join([str(random.randint(0, 9)) for i in range(length)])
