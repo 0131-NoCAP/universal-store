afb4887c63c4b47dafb878d3c1af55082be280a9
Andrew Wang <andrew@lawn-128-61-1-89.lawn.gatech.edu>
2020-11-01 16:04:10 -0500

    added checkout and barcode queries to lambda function

--- server/shopify_api_handler/lambda_function.py
+++ server/shopify_api_handler/lambda_function.py
@@ -1,15 +1,25 @@
 import json, requests, boto3
 from botocore.exceptions import ClientError
+from base64 import b64encode
 
 def lambda_handler(event, context):
     # TODO implement
     response = json.dumps(get_merchant_access_key('andrew-and-david-bridal-services.myshopify.com'))
     if event.get(api_name) == 'getStoreNames':
         response = get_store_names()
+    elif event.get('api_name') == 'createCheckout':
+        store_url = event.get('store_url')
+        items = event.get('items')
+        response = create_checkout(store_url, items)
+    elif event.get('api_name') == 'getIDFromBarcode':
+        store_url = event.get('store_url')
+        barcode = event.get('barcode')
+        response = get_id_from_barcode(store_url, barcode)
     return {
         'statusCode': 200,
         'body': response
     }
+
 
 def get_merchant_access_key(store_url):
 
@@ -23,6 +33,7 @@ def get_merchant_access_key(store_url):
     else:
         return key
 
+
 def get_store_names():
     dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
     table = dynamodb.Table('merchant_access_token-dev')
@@ -32,3 +43,132 @@ def get_store_names():
         response = table.scan(AttributesToGet=['store_url', 'store_name'])
         data.extend(response['Items'])
     return data
+
+
+def get_storefront_access_key(store_url):
+    
+    url = 'https://' + store_url + '/admin/api/2020-10/storefront_access_tokens.json'
+    headers = {
+        'X-Shopify-Access-Token': get_merchant_access_key(store_url),
+        'Content-Type': 'application/json'
+    }
+
+    data = {
+        "storefront_access_token": {
+            "title": "Test"
+        }
+    }
+
+    r = requests.post(url, json=data, headers=headers)
+    storefront_token = r.json()['storefront_access_token']['access_token']
+    return storefront_token
+
+
+def create_checkout(store_url: str, items: str):
+
+    url = 'https://' + store_url + '/api/2020-10/graphql.json'
+    storefront_token = get_storefront_access_key(store_url)
+    headers = {
+        'X-Shopify-Storefront-Access-Token': storefront_token,
+        'Content-Type': 'application/graphql'
+    }
+
+    line_items = '[{ '
+    for item in items:
+        line_items += 'variantId: "{}", quantity: {}'.format(item['id'], item['quantity'])
+    line_items += '}]'
+
+    mutation = """mutation {
+        checkoutCreate(input: {
+            lineItems: %s
+        }) {
+            userErrors{
+                field
+                message
+            }
+            checkout {
+                id
+                webUrl
+                lineItems(first:2) {
+                    edges {
+                        node {
+                            id
+                            title
+                            quantity
+                        }
+                    }
+                }
+            }
+        }
+    }""" % line_items
+
+    r = requests.post(url, data=mutation, headers=headers)
+    return r.json()
+
+
+def modify_checkout(store_url: str, items: dict, checkout_id: str):
+
+    url = 'https://' + store_url + '/api/2020-10/graphql.json'
+    storefront_token = get_storefront_access_key(store_url)
+    headers = {
+        'X-Shopify-Storefront-Access-Token': storefront_token,
+        'Content-Type': 'application/graphql'
+    }
+
+    line_items = '[{ '
+    for item in items:
+        line_items += 'variantId: "{}", quantity: {}'.format(item['id'], item['quantity'])
+    line_items += '}]'
+
+    mutation = """mutation {
+        checkoutLineItemsReplace(input: {
+            lineItems: %s, checkoutId: "%s"
+        }) {
+            userErrors{
+                field
+                message
+            }
+            checkout {
+                id
+                webUrl
+                lineItems(first:2) {
+                    edges {
+                        node {
+                            id
+                            title
+                            quantity
+                        }
+                    }
+                }
+            }
+        }
+    }""" % (line_items, checkout_id)
+
+    r = requests.post(url, data=mutation, headers=headers)
+    return r.json()
+
+
+
+def get_id_from_barcode(store_url: str, barcode: str):
+
+    url = 'https://' + store_url + '/admin/api/2020-10/graphql.json'
+    headers = {
+        'X-Shopify-Access-Token': get_merchant_access_key(store_url),
+        'Content-Type': 'application/graphql'
+    }
+
+    query = """query {
+        productVariants(query:"%s" first:1) {
+            edges {
+                node {
+                    barcode
+                    id
+                }
+            }
+        }
+    }""" % barcode
+
+    r = requests.post(url, data=query, headers=headers)
+    gid = r.json()['data']['productVariants']['edges'][0]['node']['id']
+    gid_b64 = b64encode(gid.encode('ascii')).decode('ascii')
+    return gid_b64
