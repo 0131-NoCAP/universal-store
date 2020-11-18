02e8d4c6faca0f34ed6b285e5cde6bbf98293127
charjhall <jackson.hall@gatech.edu>
2020-11-18 13:05:39 -0500

    Some refactoring for getting context

--- application/screens/CartScreen.js
+++ application/screens/CartScreen.js
@@ -7,65 +7,121 @@ import Icon from 'react-native-vector-icons/Ionicons';
 
 export default class CartScreen extends Component {
 
-  renderItem({ item, index }) {
+  // renderItem({ item, index }) {
 
-    return (
-    <View style={styles.containerItemStyle}>
-      <Image source={{url: item.product.media.edges[0].node.preview.image.originalSrc}} style={styles.imageCartStyle} />
-      <View style={styles.textCartStyle}>
-        <Text style={{ color: '#2e2f30' }}>{item.displayName}</Text>
-        <View style={styles.priceCartStyle}>
-          <Text style={{ color: '#2e2f30', fontSize: 12 }}>${item.price}</Text>
-        </View>
-      </View>
+  //   return (
+  //   <View style={styles.containerItemStyle}>
+  //     <Image source={{url: item.product.media.edges[0].node.preview.image.originalSrc}} style={styles.imageCartStyle} />
+  //     <View style={styles.textCartStyle}>
+  //       <Text style={{ color: '#2e2f30' }}>{item.displayName}</Text>
+  //       <View style={styles.priceCartStyle}>
+  //         <Text style={{ color: '#2e2f30', fontSize: 12 }}>${item.price}</Text>
+  //       </View>
+  //     </View>
 
-      <View style={styles.counterCartStyle}>
-      <Icon.Button 
-          name="ios-remove" 
-          size={20} 
-          color='#fff' 
-          backgroundColor='#fff' 
-          style={styles.removeAddButton} 
-          iconStyle={{ marginRight: 0 }}
-          //onPress={this.onPressPlus(index)}
-        />
+  //     <View style={styles.counterCartStyle}>
+  //     <Icon.Button 
+  //         name="ios-remove" 
+  //         size={20} 
+  //         color='#fff' 
+  //         backgroundColor='#fff' 
+  //         style={styles.removeAddButton} 
+  //         iconStyle={{ marginRight: 0 }}
+  //         onPress={() => {
+  //           let updatedCart = items;
 
-        <Text>{item.quantity}</Text>
+  //           setCart();
+  //         }}
+  //       />
 
-        <Icon.Button
-          name="ios-add"
-          size={20}
-          color='#fff'
-          backgroundColor='#fff'
-          style={styles.removeAddButton}
-          iconStyle={{ marginRight: 0 }}
-          //onPress={this.onPressPlus(index)}
-        />
+  //       <Text>{item.quantity}</Text>
 
-      </View>
-    </View>);
-  }
+  //       <Icon.Button
+  //         name="ios-add"
+  //         size={20}
+  //         color='#fff'
+  //         backgroundColor='#fff'
+  //         style={styles.removeAddButton}
+  //         iconStyle={{ marginRight: 0 }}
+  //         onPress={console.log(index)}
+  //       />
+
+  //     </View>
+  //   </View>);
+  // }
 
   static contextType = CartContext;
 
   render() {
     
     return (
-      <View style={{flex: 1}}>
-        <View style={styles.cartStyle}>
-          <FlatList
-            data={this.context.items}
-            renderItem={this.renderItem}
-            keyExtractor={(item) => item.id}
-          />
-          {/* <TouchableOpacity
-            onPress={() => console.log(this.context.items[0].product.media.edges[0].node.preview.image.originalSrc)}
-            style={styles.wideBtn}
-          >
-            <Text style={styles.buttonText}>TEST BUTTON</Text>
-          </TouchableOpacity> */}
-        </View>
-      </View>
+      <CartContext.Consumer>
+        {({items, setCart}) => (
+        <View style={{flex: 1}}>
+          <View style={styles.cartStyle}>
+            <FlatList
+              data={items}
+              renderItem={({ item, index }) => {  
+                <View style={styles.containerItemStyle}>
+                <Image source={{url: item.product.media.edges[0].node.preview.image.originalSrc}} style={styles.imageCartStyle} />
+                <View style={styles.textCartStyle}>
+                  <Text style={{ color: '#2e2f30' }}>{item.displayName}</Text>
+                  <View style={styles.priceCartStyle}>
+                    <Text style={{ color: '#2e2f30', fontSize: 12 }}>${item.price}</Text>
+                  </View>
+                </View>
+          
+                <View style={styles.counterCartStyle}>
+                <Icon.Button 
+                    name="ios-remove" 
+                    size={20} 
+                    color='#fff' 
+                    backgroundColor='#fff' 
+                    style={styles.removeAddButton} 
+                    iconStyle={{ marginRight: 0 }}
+                    onPress={() => {
+                      let selecteditem = items[index]
+                      if (selecteditem.inventoryQuantity > 1) selecteditem.inventoryQuantity = selecteditem.inventoryQuantity - 1;
+                      let updatedCart = items;
+                      updatedCart[index] = selecteditem;
+                      setCart(updatedCart);
+                    }}
+                  />
+          
+                  <Text>{item.quantity}</Text>
+          
+                  <Icon.Button
+                    name="ios-add"
+                    size={20}
+                    color='#fff'
+                    backgroundColor='#fff'
+                    style={styles.removeAddButton}
+                    iconStyle={{ marginRight: 0 }}
+                    onPress={() => {                     
+                      let selecteditem = items[index]
+                      selecteditem.inventoryQuantity = selecteditem.inventoryQuantity + 1;
+                      let updatedCart = items;
+                      updatedCart[index] = selecteditem;
+                      setCart(updatedCart);
+                    }}
+                  />
+          
+                </View>
+              </View>}
+            }
+              keyExtractor={(item) => item.id}
+            />
+            {/* <TouchableOpacity
+              onPress={() => console.log(items)}
+              style={styles.wideBtn}
+            >
+              <Text style={styles.buttonText}>TEST BUTTON</Text>
+            </TouchableOpacity> */}
+          </View>
+        </View>         
+      )}
+      </CartContext.Consumer>
+      
     );
   }
 }
