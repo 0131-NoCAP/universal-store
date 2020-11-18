1fa121164d41de68d45ff02ba89b0502a2e1e205
Andrew Wang <andrew@lawn-128-61-1-89.lawn.gatech.edu>
2020-11-18 15:12:54 -0500

    added modal and webview for checkout

--- application/screens/CartScreen.js
+++ application/screens/CartScreen.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import { View, FlatList, Text, Image, TouchableOpacity } from 'react-native';
+import { Button } from 'react-native-elements';
 import { landingPageStyles as styles } from '../constants/Styles';
 import { CartContext } from "../providers/cart";
 import Icon from 'react-native-vector-icons/Ionicons';
+import { WebView } from 'react-native-webview';
+import Modal from 'react-native-modal';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 // import CartFooter from '../components/checkout/CartFooter';
 
 export default class CartScreen extends Component {
+
+  state = {
+    webCheckout: false,
+  }
 
   renderItem({ item, index }) {
 
@@ -58,14 +66,54 @@ export default class CartScreen extends Component {
             renderItem={this.renderItem}
             keyExtractor={(item) => item.id}
           />
-          {/* <TouchableOpacity
-            onPress={() => console.log(this.context.items[0].product.media.edges[0].node.preview.image.originalSrc)}
+          <TouchableOpacity
+            onPress={() => {
+              // console.log(this.context.items[0].product.media.edges[0].node.preview.image.originalSrc);
+              // TODO: if you press the checkout button without items in the cart,
+              // do nothing
+              console.log("webview");
+              this.setState({
+                webCheckout: true
+              });
+            }}
             style={styles.wideBtn}
           >
             <Text style={styles.buttonText}>TEST BUTTON</Text>
-          </TouchableOpacity> */}
+          </TouchableOpacity>
         </View>
+        
+
+        <Modal
+          isVisible={ this.state.webCheckout }
+        >
+          <View style={{ flex: 1 }}>
+            <View style={{
+                flex: 1,
+                backgroundColor: '#2b2b2b'
+              }}
+            >
+              <MaterialCommunityIcons
+                name="close-box"
+                size={30}
+                style={{marginTop: 2, marginLeft: 1}}
+                color={"#fa5c5c"}
+              />
+            </View>
+          </View>
+
+          <View style={{ flex: 19 }}>
+            <WebView
+              source={{ uri: 'https://shopify.com/partners' }}
+            />
+          </View>
+        </Modal>
       </View>
     );
+  }
+
+  exitPopup = () => {
+    this.setState({
+      webCheckout: false
+    })
   }
 }
